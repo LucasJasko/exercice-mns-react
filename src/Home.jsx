d3c7c83b6e1a12724f59e17c1cc80da8a7dfccc4
LucasJasko <lucasjaskowiak@yahoo.fr>
2025-07-16 16:33:58 +0200

    feat: ajout d'un loader pour le chargement des informations de la databse distante

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,35 +10,57 @@ const Home = ({ disconnectUser }) => {
   const [snippetList, setSnippetList] = useState([]);
   const [keyword, setKeyword] = useState("");
   const [userInfos, setUserInfos] = useState({});
-  const [userPseudo, setUserPseudo] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    let isUserInfos = false;
+    let isSnippetList = false;
     const user = getAuth().currentUser;
     setUserInfos(user);
 
     onValue(ref(getDatabase(), `${user.uid}/userInfos/pseudo`), (snapshot) => {
-      if (snapshot.val()) setUserPseudo(snapshot.val());
+      if (snapshot.val()) {
+        setUserInfos({ ...userInfos, pseudo: snapshot.val() });
+        isUserInfos = true;
+        if (isUserInfos && isSnippetList) {
+          setIsLoading(false);
+        }
+      }
     });
 
     onValue(ref(getDatabase(), `${user.uid}/snippetList`), (snapshot) => {
-      if (snapshot.val()) setSnippetList(snapshot.val());
+      if (snapshot.val()) {
+        setSnippetList(snapshot.val());
+        isSnippetList = true;
+        if (isUserInfos && isSnippetList) {
+          setIsLoading(false);
+        }
+      }
     });
   }, []);
 
   return (
     <div className="main">
-      <div className="topbar">
-        <p className="topbar-left">Connecté en tant que {userPseudo}</p>
-        <button className="topbar-right" onClick={disconnectUser}>
-          Déconnexion
-        </button>
-      </div>
-      <h1>Snippet collector</h1>
-      <Search setKeyword={setKeyword} />
-      <span className="line"></span>
-      <Form userInfos={userInfos} snippetList={snippetList} setSnippetList={setSnippetList} />
-      <span className="line"></span>
-      <List userInfos={userInfos} snippetList={snippetList} setSnippetList={setSnippetList} keyword={keyword} />
+      {isLoading ? (
+        <div className="loader-container">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <>
+          <div className="topbar">
+            <p className="topbar-left">Connecté en tant que {userInfos.pseudo}</p>
+            <button className="topbar-right" onClick={disconnectUser}>
+              Déconnexion
+            </button>
+          </div>
+          <h1>Snippet collector</h1>
+          <Search setKeyword={setKeyword} />
+          <span className="line"></span>
+          <Form userInfos={userInfos} snippetList={snippetList} setSnippetList={setSnippetList} />
+          <span className="line"></span>
+          <List userInfos={userInfos} snippetList={snippetList} setSnippetList={setSnippetList} keyword={keyword} />
+        </>
+      )}
     </div>
   );
 };
